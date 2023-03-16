15441813093ca08068e059b818f6d655ef1ecaeb
WenhaoJiang <582424296@qq.com>
2023-03-16 20:46:53 -0300

    change z to 4

--- hello2.java
+++ hello2.java
@@ -1,9 +1,8 @@
 public class hello2 {
+    private String name;
     public static void main(String[] args) {
         System.out.println("hhell");
-
-        int x=2;
-
-        int y=3;
+    }
+    public hello2(){
     }
 }
